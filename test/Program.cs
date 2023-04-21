4862867f2fa5aea3c34334a18d77051187312f58
JohnyDevp <holan.jan@outlook.com>
2023-04-21 16:24:35 +0200

    added c sharp code snippet and one wave for testing purposes

---
+++ test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Net;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            string data = File.ReadAllText("./wave1422307.json");
+            WebClient client = new WebClient();
+            // Optionally specify an encoding for uploading and downloading strings.
+            client.Encoding = System.Text.Encoding.UTF8;
+            // Upload the data.
+            string reply = client.UploadString("http://0.0.0.0:8080/input_data", data);
+            // Display the server's response.
+            Console.WriteLine(reply);
+        }
+    }
+}
